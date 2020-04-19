17b856f897fa610b32cb9bbdadc7461820544536
Sohan Shirodkar <sohan1998@github.com>
2020-04-20 00:53:12 +0530

    Fixed error with Environment Variable

--- App/dbconnect.py
+++ App/dbconnect.py
@@ -5,7 +5,7 @@ HOST     = os.environ.get('RDS_HOSTNAME') or 'localhost'
 USER     = os.environ.get('RDS_USERNAME') or 'root'
 PASSWD   = os.environ.get('RDS_PASSWORD') or 'c1o2l3d4'
 DATABASE = os.environ.get('RDS_DB_NAME') or 'society_mysqldb'
-PORT     = os.environ.get('RDS_PORT') or 3306
+PORT     = int(os.environ.get('RDS_PORT')) or 3306
 def connection():
 	conn = pymysql.connect(host = HOST, user = USER, passwd = PASSWD, db = DATABASE, port = PORT)
 	cursor = conn.cursor()
